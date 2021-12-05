a6c53c3bfcac04ca20840b584c45c777642cf545
Moss Wells <moss@Jeremys-MacBook-Pro.local>
2021-12-05 21:39:25 +1300

    golf

--- 5/5g.rb
+++ 5/5g.rb
@@ -1,4 +1,4 @@
 e=[0,1];puts File.readlines("input.txt").map{|l|l.chomp.split(/,|\s+\->\s+/).map(&:to_i)}
   .select{|l|e.any?{|p|l[p]==l[p+2]}}
-  .flat_map{|l|d=e.map{|p|l[p+2]<=>l[p]};(e.map{|p|(l[p]-l[p+2]).abs}.max+1).times.map{|t|e.map{|p|l[p]+t*d[p]}}}
-  .reduce({}){|h,p|h[p]=h[p].to_i+1;h}.select{|_,v|v>1}.size
+  .reduce({}){|h,l|d=e.map{|p|l[p+2]<=>l[p]};(e.map{|p|(l[p]-l[p+2]).abs}.max+1).times.map{|t|p=e.map{|p|l[p]+t*d[p]};h[p]=h[p].to_i+1};h}
+  .select{|_,v|v>1}.size
