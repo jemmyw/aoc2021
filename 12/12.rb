3267be983cb10a9887fd19e8bde633a3f9c0b941
Jeremy Wells <jemmyw@gmail.com>
2021-12-12 20:32:38 +1300

    Simplify 12

--- 12/12.rb
+++ 12/12.rb
@@ -1,55 +1,32 @@
-conns = File.readlines('input.txt').map { |line| line.chomp.split(/-/) }
+conns =
+  File
+    .readlines('input.txt')
+    .map { |line| line.chomp.split(/-/) }
+    .reduce(Hash.new { |h, k| h[k] = [] }) do |acc, (a, b)|
+      acc[a] << b
+      acc[b] << a
+      acc
+    end
 
 paths = []
-
-class Cave
-  attr_reader :name, :conns
-
-  def initialize(name)
-    @name = name
-    @small = name.downcase == name
-    @conns = []
-  end
-
-  def small?
-    @small
-  end
-
-  def <<(other)
-    @conns << other
-  end
-end
-
-caves =
-  conns.reduce(
-    Hash.new { |hash, key| hash[key] = Cave.new(key) },
-  ) do |acc, (a, b)|
-    acc[a] << b
-    acc[b] << a
-    acc
-  end
-
-start_cave = caves['start']
-end_cave = caves['end']
-
 to_explore = [['start']]
 
 while to_explore.any?
   partial_path = to_explore.shift
-  current_cave = caves[partial_path.last]
+  current_cave = partial_path.last
 
-  if current_cave.name == "end"
+  if current_cave == 'end'
     paths << partial_path
     next
   end
 
   next_caves =
-    current_cave.conns.reject { |c| c.downcase == c && partial_path.include?(c) }
+    conns[current_cave].reject do |c|
+      c.downcase == c && partial_path.include?(c)
+    end
 
   to_explore.push(*next_caves.map { |c| partial_path + [c] })
 end
 
-paths.each do |path|
-  puts path.join('->')
-end
+paths.each { |path| puts path.join('->') }
 puts paths.length
